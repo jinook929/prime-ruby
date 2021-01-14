7c8a4ba2f0bf60dfd4120422b0195d4cafc0eba7
jinook929 <jinook929@gmail.com>
2021-01-13 23:52:28 -0500

    commit tree 7223ad8a5026c45be438d494ad2ed1d99c8f6ee7

--- prime.rb
+++ prime.rb
@@ -1,1 +1,36 @@
 # Add  code here!
+def prime?(num)
+    if num <= 1
+        return false
+    end
+    
+    (2..(num - 1)).to_a.each {|n|
+        if num % n == 0
+            return false
+        end
+    }
+    return true
+end
+
+# def prime?(num)
+#     if num <= 1
+#         return false
+#     end
+#     if num == 2
+#         return true
+#     elsif num % 2 == 0
+#         return false
+#     else
+#         odd_nums = (3..(num - 1)).to_a.delete_if { |a| a.even? }
+#         odd_nums.each {|n|
+#             if num % n == 0
+#                 return false
+#             end
+#         }
+#     end
+#     return true
+# end
+
+# n = 901015
+# p n
+# p prime?(n)
